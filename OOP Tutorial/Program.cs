dff5256f8c032e3c9d342d2215fab5d96132ebd5
leighirving <leighann.irving@gmail.com>
2021-07-19 15:22:28 -0400

    add OOP Tutorial files

---
+++ OOP Tutorial/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOP_Tutorial
+{
+    class Car
+    {
+        string color;
+        int maxSpeed;
+    }
+
+    // Interface
+    interface IAnimal
+    {
+        void animalSound(); // interface method (does not have a body)
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    interface IFirstInterface
+    {
+        void myMethod(); // interface method
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod(); // interface method
+    }
+
+    // Implement multiple interfaces
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Some text..");
+        }
+        public void myOtherMethod()
+        {
+            Console.WriteLine("Some other text...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+        }
+    }
+
+}
