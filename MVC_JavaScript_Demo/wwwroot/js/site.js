98dd72619c58aceccdab38fe32109729bad641f8
leighirving <leighann.irving@gmail.com>
2021-07-19 15:14:28 -0400

    add MVC_JavaScript_JQuery_Chart_Ajax files

---
+++ MVC_JavaScript_Demo/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Write your JavaScript code.
+
+function onBegin(target) {
+  var resultsPlace = $(target);
+  resultsPlace.removeClass("hidden");
+  resultsPlace.addClass("show");
+}
+
+function onSuccess(data, target) {
+  $(target).html(data);
+};
